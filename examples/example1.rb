eb4c8317ea6a3fc36aff31f940f9a39a84db69a1
Brian Reily <breily@gmail.com>
2009-01-09 23:39:08 -0500

    added comment method

--- examples/example1.rb
+++ examples/example1.rb
@@ -15,14 +15,18 @@
 #
 # Use this Ruby:
 
+comment "The 'Hello, World' of Ruby2Make"
+
 vars :FLAGS => "-g"
 
 rule "hello", :depends => "hello.o" do
+    comment "Compiles object code to an executable"
     compile :output => "hello"
     echo "Done compiling, run ./hello"
 end
 
 rule "hello.o", :d => ["hello.c", "hello.h"] do
+    comment "Compiles C code to object code"
     compile :to_obj
 end
 
